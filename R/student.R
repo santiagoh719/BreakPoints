e4b08255e823be4c4e6c8426ecbbc046afd42004
santiagoh719 <43649362+santiagoh719@users.noreply.github.com>
2018-12-07 17:06:31 -0300

    Update student.R

--- R/student.R
+++ R/student.R
@@ -11,7 +11,7 @@ stu <- function(serie,n_period=10){
     aux1 <- serie[1:i]
     aux2 <- serie[(i+1):n]
     p <- t.test(aux1,aux2, paired = F, var.equal = F)$p.value
-    if(p < p_v){
+    if(p <= p_v){
       p_v <- p
       i_break <- i+1
     }
